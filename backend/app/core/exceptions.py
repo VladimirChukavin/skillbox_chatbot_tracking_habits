fea16b75157ea53dbff4bc410da49d6b9d29a5a0
VladimirChukavin <dartmorgot@yandex.ru>
2026-08-06 21:55:57 +1000

    Создал файл exceptions.py.

---
+++ backend/app/core/exceptions.py
@@ -0,0 +1,21 @@
+class AppException(Exception):
+    status_code: int
+    error_type: str
+    error_message: str
+
+    def __init__(self, error_message: str | None = None) -> None:
+        if error_message is not None:
+            self.error_message = error_message
+        super().__init__(self.error_message)
+
+
+class AuthenticationError(AppException):
+    status_code = 401
+    error_type = "authentication_error"
+    error_message = "Authentication failed"
+
+
+class ValidationError(AppException):
+    status_code = 400
+    error_type = "validation_error"
+    error_message = "Validation failed"
