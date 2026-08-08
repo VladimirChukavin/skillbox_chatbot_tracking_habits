74e15272e9918eee0c7a6f394bae4ff2f147db3a
VladimirChukavin <dartmorgot@yandex.ru>
2026-08-08 20:01:40 +1000

    Добавил в код классы ошибок.

--- backend/app/core/exceptions.py
+++ backend/app/core/exceptions.py
@@ -19,3 +19,21 @@ class ValidationError(AppException):
     status_code = 400
     error_type = "validation_error"
     error_message = "Validation failed"
+
+
+class NotFoundError(AppException):
+    status_code = 404
+    error_type = "not_found"
+    error_message = "Entity not found"
+
+
+class ForbiddenError(AppException):
+    status_code = 403
+    error_type = "forbidden"
+    error_message = "Access denied"
+
+
+class ConflictError(AppException):
+    status_code = 409
+    error_type = "conflict_error"
+    error_message = "Conflict"
