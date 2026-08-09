fec865a555360e4af3d5b16288574942538b587e
VladimirChukavin <dartmorgot@yandex.ru>
2026-08-09 20:45:30 +1000

    Исправил название переменной.

--- backend/app/config.py
+++ backend/app/config.py
@@ -8,7 +8,7 @@ class Settings(BaseSettings):
 
     postgres_user: str
     postgres_password: str
-    postgres_db: str
+    postgres_name: str
     postgres_host: str = "localhost"
     postgres_port: int = 5432
 
@@ -24,14 +24,14 @@ class Settings(BaseSettings):
     def database_url(self) -> str:
         return (
             f"postgresql+asyncpg://{self.postgres_user}:{self.postgres_password}"
-            f"@{self.postgres_host}:{self.postgres_port}/{self.postgres_db}"
+            f"@{self.postgres_host}:{self.postgres_port}/{self.postgres_name}"
         )
 
     @property
     def sync_database_url(self) -> str:
         return (
             f"postgresql+psycopg2://{self.postgres_user}:{self.postgres_password}"
-            f"@{self.postgres_host}:{self.postgres_port}/{self.postgres_db}"
+            f"@{self.postgres_host}:{self.postgres_port}/{self.postgres_name}"
         )
 
 
