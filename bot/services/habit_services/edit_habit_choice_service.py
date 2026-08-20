c0044018e068fca0490c8b51c1ca9ee3da205cfb
VladimirChukavin <dartmorgot@yandex.ru>
2026-08-21 00:00:31 +1000

    Создал файл edit_habit_choice_service.py.

---
+++ bot/services/habit_services/edit_habit_choice_service.py
@@ -0,0 +1,25 @@
+from telebot import TeleBot
+from telebot.types import CallbackQuery
+
+from bot.keyboards import build_edit_fields_keyboard
+from bot.states import EditHabitStates
+
+
+def show_edit_habit_choice(bot: TeleBot, call: CallbackQuery) -> None:
+    habit_id = int(call.data.split(":")[1])
+    telegram_id = call.from_user.id
+
+    with bot.retrieve_data(telegram_id, call.message.chat.id) as data:
+        data["editing_habit_id"] = habit_id
+
+    bot.set_state(
+        telegram_id,
+        EditHabitStates.waiting_for_field_choice,
+        call.message.chat.id,
+    )
+    bot.edit_message_text(
+        "Что вы хотите изменить?",
+        chat_id=call.message.chat.id,
+        message_id=call.message.message_id,
+        reply_markup=build_edit_fields_keyboard(),
+    )
