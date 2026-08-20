4fa94e348f07880ffcbcd63889dd9bbe722afd11
VladimirChukavin <dartmorgot@yandex.ru>
2026-08-20 23:58:01 +1000

    Создал файл add_habit_service.py.

---
+++ bot/services/habit_services/add_habit_service.py
@@ -0,0 +1,12 @@
+from telebot import TeleBot
+
+from bot.states import AddHabitStates
+from bot.storage import token_storage
+
+
+def show_add_habit(bot: TeleBot, telegram_id: int, chat_id: int) -> None:
+    if token_storage.get_tokens(telegram_id) is None:
+        return
+
+    bot.set_state(telegram_id, AddHabitStates.waiting_for_title, chat_id)
+    bot.send_message(telegram_id, "Введите название привычки:")
