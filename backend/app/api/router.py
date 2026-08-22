99ad795415f45d9a9ebd48ddd8aa5432d7e1fe66
VladimirChukavin <dartmorgot@yandex.ru>
2026-08-22 22:14:36 +1000

    Создал файл router.py.

---
+++ backend/app/api/router.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+
+from app.api.auth_routers import auth_router
+from app.api.habits_routers import habits_router
+from app.api.user_routers import user_router
+
+main_router = APIRouter()
+
+main_router.include_router(auth_router.main_auth_router)
+main_router.include_router(habits_router.main_habits_router)
+main_router.include_router(user_router.router)
