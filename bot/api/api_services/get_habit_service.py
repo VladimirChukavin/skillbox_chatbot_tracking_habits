2ee3b8af8fcdd4a81c048fff4ec963b82471fe10
VladimirChukavin <dartmorgot@yandex.ru>
2026-08-22 16:51:23 +1000

    Создал файл get_habit_service.py.

---
+++ bot/api/api_services/get_habit_service.py
@@ -0,0 +1,16 @@
+from requests import Session
+
+from bot.api.api_services.authorized_request_service import _authorized_request
+
+
+def get_habit_service(
+    session: Session,
+    base_url: str,
+    method: str,
+    telegram_id: int,
+    habit_id: int,
+) -> dict | None:
+    endpoint = f"/habits/{habit_id}"
+    response = _authorized_request(session, base_url, method, telegram_id, endpoint)
+
+    return response.json() if response and response.status_code == 200 else None
