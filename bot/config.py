d5a1312dc1822cc56610c3eea57b3e09b70d4d8a
VladimirChukavin <dartmorgot@yandex.ru>
2026-08-22 16:47:27 +1000

    Добавил список дефолтных команд.

--- bot/config.py
+++ bot/config.py
@@ -1,5 +1,16 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+DEFAULT_COMMANDS = (
+    ("start", "Запустить бота"),
+    ("login", "Введите пароль для получения токена на новую сессию"),
+    ("add_habit", "Введите название привычки"),
+    ("habits", "Ваши привычки"),
+    ("edit_habit", "Редактировать привычку"),
+    ("set_reminder", "Установить напоминание"),
+    ("habit_stats", "Статистика привычек"),
+    ("track_habit", "Выберите привычку для трекинга"),
+)
+
 
 class BotSettings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
