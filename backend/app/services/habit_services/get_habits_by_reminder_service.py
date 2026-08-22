532b23f22dab0371559cc1ad203d3451f11f9e81
VladimirChukavin <dartmorgot@yandex.ru>
2026-08-22 22:05:14 +1000

    Создал файл get_habits_by_reminder_service.py.

---
+++ backend/app/services/habit_services/get_habits_by_reminder_service.py
@@ -0,0 +1,33 @@
+import datetime
+
+import sqlalchemy
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.models.habit_model import Habit
+from app.models import User
+
+
+async def get_habits_by_reminder(
+    session: AsyncSession, reminder_time: datetime.time
+) -> list[dict]:
+    statement = (
+        select(Habit, User.telegram_id)
+        .join(User, Habit.user_id == User.id)
+        .where(Habit.is_active.is_(True))
+        .where(Habit.reminder_time.isnot(None))
+        .where(sqlalchemy.extract("hour", Habit.reminder_time) == reminder_time.hour)
+        .where(
+            sqlalchemy.extract("minute", Habit.reminder_time) == reminder_time.minute
+        )
+    )
+    result = await session.execute(statement)
+
+    return [
+        {
+            "telegram_id": row.telegram_id,
+            "habit_id": row.Habit.id,
+            "title": row.Habit.title,
+        }
+        for row in result
+    ]
