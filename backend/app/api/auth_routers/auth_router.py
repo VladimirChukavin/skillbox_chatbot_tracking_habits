74561116421c1337ee4edc1f5e28d6a04d927b8b
VladimirChukavin <dartmorgot@yandex.ru>
2026-08-22 21:51:26 +1000

    Создал файл auth_router.py.

---
+++ backend/app/api/auth_routers/auth_router.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter
+
+from app.api.auth_routers import (
+    user_register_router,
+    login_user_router,
+    login_oauth_form_router,
+    refresh_access_token_router,
+)
+
+main_auth_router = APIRouter(prefix="/auth", tags=["auth"])
+
+main_auth_router.include_router(user_register_router.router)
+main_auth_router.include_router(login_user_router.router)
+main_auth_router.include_router(login_oauth_form_router.router)
+main_auth_router.include_router(refresh_access_token_router.router)
