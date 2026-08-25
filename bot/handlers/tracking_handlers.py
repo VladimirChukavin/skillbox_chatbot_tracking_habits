68d25ec764df9c43309608953d06994e99bd48e1
VladimirChukavin <dartmorgot@yandex.ru>
2026-08-25 14:51:22 +1000

    Добавил описание функций.

--- bot/handlers/tracking_handlers.py
+++ bot/handlers/tracking_handlers.py
@@ -1,3 +1,10 @@
+"""
+Обработчики для отметки выполнения привычек (трекинг).
+
+Содержит функцию регистрации хендлеров для команды /track_habit,
+выбора привычки и отметки её выполнения/невыполнения через callback-кнопки.
+"""
+
 from telebot import TeleBot
 from telebot.types import Message, CallbackQuery
 
@@ -8,18 +15,63 @@ from bot.services.tracking_services.track_skip_service import show_track_skip
 
 
 def register_tracking_handlers(bot: TeleBot) -> None:
+    """
+    Зарегистрировать обработчики для трекинга выполнения привычек.
+
+    :param bot: Экземпляр Telegram-бота
+    :type bot: TeleBot
+    :return: Ничего не возвращает
+    :rtype: None
+    """
+
     @bot.message_handler(commands=["track_habit"])
     def handle_track_habit(message: Message) -> None:
+        """
+        Обработчик команды /track_habit. Показывает список привычек для отметки.
+
+        :param message: Входящее сообщение от пользователя
+        :type message: Message
+        :return: Ничего не возвращает
+        :rtype: None
+        """
+
         show_track_habit(bot, message.from_user.id, message.chat.id)
 
     @bot.callback_query_handler(func=lambda call: call.data.startswith("track:"))
     def handle_track_choice(call: CallbackQuery) -> None:
+        """
+        Обработчик выбора привычки для отметки (callback "track:").
+
+        :param call: Callback-запрос от inline-клавиатуры
+        :type call: CallbackQuery
+        :return: Ничего не возвращает
+        :rtype: None
+        """
+
         show_track_choice(bot, call)
 
     @bot.callback_query_handler(func=lambda call: call.data.startswith("track_done:"))
     def handle_track_done(call: CallbackQuery) -> None:
+        """
+        Обработчик отметки выполнения привычки (callback "track_done:").
+
+        :param call: Callback-запрос от inline-клавиатуры
+        :type call: CallbackQuery
+        :return: Ничего не возвращает
+        :rtype: None
+        """
+
         show_track_done(bot, call)
 
     @bot.callback_query_handler(func=lambda call: call.data.startswith("track_skip:"))
     def handle_track_skip(call: CallbackQuery) -> None:
+        """
+        Обработчик отметки невыполнения привычки (callback "track_skip:").
+
+        :param call: Callback-запрос от inline-клавиатуры
+        :type call: CallbackQuery
+        :return: Ничего не возвращает
+        :rtype: None
+        """
+
         show_track_skip(bot, call)
