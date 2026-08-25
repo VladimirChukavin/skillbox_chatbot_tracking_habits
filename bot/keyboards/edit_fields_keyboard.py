85f795548fe3b1bea416adcc48d287fd742fabec
VladimirChukavin <dartmorgot@yandex.ru>
2026-08-25 14:43:59 +1000

    Добавил описание функции.

--- bot/keyboards/edit_fields_keyboard.py
+++ bot/keyboards/edit_fields_keyboard.py
@@ -1,7 +1,24 @@
+"""
+Клавиатура для выбора поля привычки при редактировании.
+
+Содержит функцию для построения inline-клавиатуры, позволяющей
+пользователю выбрать, какое поле привычки обновить или удалить её.
+"""
+
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 def build_edit_fields_keyboard() -> InlineKeyboardMarkup:
+    """
+    Построить клавиатуру выбора поля для редактирования привычки.
+
+    Создаёт кнопки для обновления названия, описания, цели, срока,
+    а также для полного удаления привычки.
+
+    :return: Объект inline-клавиатуры с кнопками полей
+    :rtype: InlineKeyboardMarkup
+    """
+
     keyboard = InlineKeyboardMarkup()
     fields = [
         ("Название", "field:title"),
