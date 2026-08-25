77fd2a6a16d811c95680a775005356802a7ecffd
VladimirChukavin <dartmorgot@yandex.ru>
2026-08-25 14:51:02 +1000

    Добавил описание функции.

--- bot/keyboards/track_keyboard.py
+++ bot/keyboards/track_keyboard.py
@@ -1,7 +1,27 @@
+"""
+Inline-клавиатура для отметки выполнения привычки.
+
+Содержит функцию для построения клавиатуры с кнопками "Выполнено"
+и "Не выполнено" для трекинга привычек.
+"""
+
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 def build_track_keyboard(habit_id: int) -> InlineKeyboardMarkup:
+    """
+    Построить клавиатуру для отметки выполнения привычки.
+
+    Создаёт две кнопки в одной строке: для отметки выполнения
+    (callback "track_done:") и невыполнения (callback "track_skip:").
+    В callback_data передаётся ID привычки.
+
+    :param habit_id: Идентификатор привычки для отметки
+    :type habit_id: int
+    :return: Объект inline-клавиатуры с кнопками отметки
+    :rtype: InlineKeyboardMarkup
+    """
+
     keyboard = InlineKeyboardMarkup()
     keyboard.row(
         InlineKeyboardButton(
