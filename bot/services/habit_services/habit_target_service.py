739851f98cbfb7418fc5169df86a8d874e6dcd72
VladimirChukavin <dartmorgot@yandex.ru>
2026-08-25 14:45:28 +1000

    Добавил описание функции.

--- bot/services/habit_services/habit_target_service.py
+++ bot/services/habit_services/habit_target_service.py
@@ -1,3 +1,11 @@
+"""
+Сервис обработки ввода цели привычки.
+
+Содержит функцию, которая сохраняет введённую цель в данных
+состояния (FSM) и переводит пользователя к следующему шагу
+формы создания привычки — вводу срока выполнения в днях.
+"""
+
 from telebot import TeleBot
 from telebot.types import Message
 
@@ -5,6 +13,24 @@ from bot.states import AddHabitStates
 
 
 def show_habit_target(bot: TeleBot, message: Message) -> None:
+    """
+    Обработать ввод цели привычки.
+
+    Извлекает текст цели из сообщения и сохраняет его в данных
+    состояния (FSM) под ключом "target_description". Если
+    пользователь ввёл "-" (пропуск), в FSM сохраняется None.
+    Переводит пользователя в состояние ожидания ввода срока
+    выполнения в днях (AddHabitStates.waiting_for_target_days)
+    и отправляет соответствующее сообщение.
+
+    :param bot: Экземпляр Telegram-бота
+    :type bot: TeleBot
+    :param message: Входящее сообщение с целью привычки
+    :type message: Message
+    :return: Ничего не возвращает
+    :rtype: None
+    """
+
     target_description = message.text.strip() if message.text else ""
 
     with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
