f1c1b7eb5c594e8976cec4b6aad314ff248dc1e5
VladimirChukavin <dartmorgot@yandex.ru>
2026-08-25 14:43:37 +1000

    Добавил описание функции.

--- bot/services/habit_services/delete_habit_choice_service.py
+++ bot/services/habit_services/delete_habit_choice_service.py
@@ -1,3 +1,12 @@
+"""
+Сервис обработки выбора привычки для удаления.
+
+Содержит функцию, которая вызывается при нажатии inline-кнопки
+удаления конкретной привычки. Функция извлекает идентификатор
+привычки из callback-данных, отправляет запрос на удаление
+через API-клиент и обновляет сообщение с результатом.
+"""
+
 from telebot import TeleBot
 from telebot.types import CallbackQuery
 
@@ -5,6 +14,23 @@ from bot.api.api_client import api_client
 
 
 def show_delete_habit_choice(bot: TeleBot, call: CallbackQuery) -> None:
+    """
+    Обработать выбор привычки для удаления.
+
+    Извлекает идентификатор привычки из callback-данных формата
+    "delete:<habit_id>". Отправляет запрос на удаление через
+    API-клиент. При успешном удалении сбрасывает FSM-состояние
+    пользователя и заменяет текст исходного сообщения на уведомление
+    об успехе. При неудаче отправляет в чат сообщение об ошибке.
+
+    :param bot: Экземпляр Telegram-бота
+    :type bot: TeleBot
+    :param call: Callback-запрос от inline-кнопки выбора привычки
+    :type call: CallbackQuery
+    :return: Ничего не возвращает
+    :rtype: None
+    """
+
     habit_id = int(call.data.split(":")[1])
     telegram_id = call.from_user.id
 
