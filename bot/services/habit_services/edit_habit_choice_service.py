2c848d0534b412d0bc02b8bd4c1c4ffc8531c987
VladimirChukavin <dartmorgot@yandex.ru>
2026-08-25 14:44:05 +1000

    Добавил описание функции.

--- bot/services/habit_services/edit_habit_choice_service.py
+++ bot/services/habit_services/edit_habit_choice_service.py
@@ -1,3 +1,12 @@
+"""
+Сервис обработки выбора привычки для редактирования.
+
+Содержит функцию, которая вызывается при нажатии inline-кнопки
+редактирования конкретной привычки. Функция извлекает идентификатор
+привычки из callback-данных, сохраняет его в FSM-данных и
+отображает inline-клавиатуру для выбора редактируемого поля.
+"""
+
 from telebot import TeleBot
 from telebot.types import CallbackQuery
 
@@ -6,6 +15,25 @@ from bot.states import EditHabitStates
 
 
 def show_edit_habit_choice(bot: TeleBot, call: CallbackQuery) -> None:
+    """
+    Обработать выбор привычки для редактирования.
+
+    Извлекает идентификатор привычки из callback-данных формата
+    "edit:<habit_id>" и сохраняет его в данных состояния (FSM)
+    под ключом "editing_habit_id". Переводит пользователя в
+    состояние ожидания выбора поля
+    (EditHabitStates.waiting_for_field_choice) и заменяет
+    текст исходного сообщения на запрос «Что вы хотите изменить?»
+    с inline-клавиатурой.
+
+    :param bot: Экземпляр Telegram-бота
+    :type bot: TeleBot
+    :param call: Callback-запрос от inline-кнопки выбора привычки
+    :type call: CallbackQuery
+    :return: Ничего не возвращает
+    :rtype: None
+    """
+
     habit_id = int(call.data.split(":")[1])
     telegram_id = call.from_user.id
 
