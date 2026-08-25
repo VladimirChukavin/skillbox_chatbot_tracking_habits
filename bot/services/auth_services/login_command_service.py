616ccf9441bb7d61e3cce31df00e1c0e397a1fa1
VladimirChukavin <dartmorgot@yandex.ru>
2026-08-25 14:47:08 +1000

    Добавил описание функции.

--- bot/services/auth_services/login_command_service.py
+++ bot/services/auth_services/login_command_service.py
@@ -1,3 +1,10 @@
+"""
+Сервис для обработки команды /login.
+
+Содержит функцию, которая переводит пользователя в состояние ожидания
+пароля (FSM) для последующей аутентификации на бэкенде.
+"""
+
 from telebot import TeleBot
 from telebot.types import Message
 
@@ -5,6 +12,20 @@ from bot.states import LoginStates
 
 
 def show_login_command(bot: TeleBot, message: Message) -> None:
+    """
+    Инициировать процесс входа пользователя в систему.
+
+    Устанавливает состояние (FSM) ожидания ввода пароля и отправляет
+    пользователю соответствующее сообщение.
+
+    :param bot: Экземпляр Telegram-бота
+    :type bot: TeleBot
+    :param message: Входящее сообщение (команда /login)
+    :type message: Message
+    :return: Ничего не возвращает
+    :rtype: None
+    """
+
     telegram_id = message.from_user.id
     bot.set_state(
         telegram_id,
