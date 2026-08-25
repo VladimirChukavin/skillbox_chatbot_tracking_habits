4f369f9ce436f7b423bdd1b080a18c5bffed4008
VladimirChukavin <dartmorgot@yandex.ru>
2026-08-25 14:48:30 +1000

    Добавил описание функции.

--- bot/services/reminder_services/reminder_choice_service.py
+++ bot/services/reminder_services/reminder_choice_service.py
@@ -1,3 +1,12 @@
+"""
+Сервис обработки выбора привычки для установки напоминания.
+
+Содержит функцию, которая вызывается при нажатии inline-кнопки
+выбора конкретной привычки. Функция извлекает идентификатор
+привычки из callback-данных, сохраняет его в FSM-данных и
+переводит пользователя к вводу времени напоминания.
+"""
+
 from telebot import TeleBot
 from telebot.types import CallbackQuery
 
@@ -5,6 +14,24 @@ from bot.states import ReminderStates
 
 
 def show_reminder_choice(bot: TeleBot, call: CallbackQuery) -> None:
+    """
+    Обработать выбор привычки для установки напоминания.
+
+    Извлекает идентификатор привычки из callback-данных формата
+    "reminder:<habit_id>" и сохраняет его в данных состояния
+    (FSM) под ключом "reminder_habit_id". Переводит
+    пользователя в состояние ожидания ввода времени напоминания
+    (ReminderStates.waiting_for_time) и заменяет текст
+    исходного сообщения на запрос времени.
+
+    :param bot: Экземпляр Telegram-бота
+    :type bot: TeleBot
+    :param call: Callback-запрос от inline-кнопки выбора привычки
+    :type call: CallbackQuery
+    :return: Ничего не возвращает
+    :rtype: None
+    """
+
     habit_id = int(call.data.split(":")[1])
     telegram_id = call.from_user.id
 
