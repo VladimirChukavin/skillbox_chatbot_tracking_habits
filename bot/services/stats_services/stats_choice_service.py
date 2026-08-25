4be8bb5b8367ab5d5d470fba0a785d43b9f014e5
VladimirChukavin <dartmorgot@yandex.ru>
2026-08-25 14:49:31 +1000

    Добавил описание функции.

--- bot/services/stats_services/stats_choice_service.py
+++ bot/services/stats_services/stats_choice_service.py
@@ -1,3 +1,12 @@
+"""
+Сервис отображения статистики по выбранной привычке.
+
+Содержит функцию, которая вызывается при нажатии inline-кнопки
+выбора привычки. Функция извлекает идентификатор привычки из
+callback-данных, запрашивает статистику через API-клиент и
+формирует читаемое текстовое сообщение с прогрессом выполнения.
+"""
+
 from telebot import TeleBot
 from telebot.types import CallbackQuery
 
@@ -5,6 +14,28 @@ from bot.api.api_client import api_client
 
 
 def show_stats_choice(bot: TeleBot, call: CallbackQuery) -> None:
+    """
+    Обработать выбор привычки для просмотра статистики.
+
+    Извлекает идентификатор привычки из callback-данных формата
+    "stats:<habit_id>" и запрашивает статистику через
+    api_client.get_habit_stats. Если запрос вернул None —
+    отвечает на callback уведомлением об ошибке и прерывает
+    обработку.
+
+    При успехе формирует текстовое сообщение, содержащее название
+    привычки, количество выполненных дней из целевого, процент
+    прогресса и отметку о выполнении за сегодня.
+    Заменяет текст исходного сообщения на сформированное.
+
+    :param bot: Экземпляр Telegram-бота
+    :type bot: TeleBot
+    :param call: Callback-запрос от inline-кнопки выбора привычки
+    :type call: CallbackQuery
+    :return: Ничего не возвращает
+    :rtype: None
+    """
+
     habit_id = int(call.data.split(":")[1])
     telegram_id = call.from_user.id
     stats = api_client.get_habit_stats(telegram_id, habit_id)
