1c703bda546d5854759ef2a15a3cac8a83b5778e
VladimirChukavin <dartmorgot@yandex.ru>
2026-08-25 14:43:17 +1000

    Добавил описание классу.

--- bot/config.py
+++ bot/config.py
@@ -1,3 +1,12 @@
+"""
+Конфигурация бота на базе pydantic-settings.
+
+Содержит кортеж команд бота по умолчанию, класс настроек
+BotSettings, загружающий параметры из переменных окружения
+(файл .env), и глобальный экземпляр настроек bot_settings,
+используемый для всего проекта.
+"""
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 DEFAULT_COMMANDS = (
@@ -14,6 +23,22 @@ DEFAULT_COMMANDS = (
 
 
 class BotSettings(BaseSettings):
+    """
+    Настройки бота, загружаемые из переменных окружения.
+
+    Чтение переменных производится из файла .env.
+    Обязательные параметры (telegram_token, api_base_url)
+    должны быть заданы в окружении; остальные имеют значения
+    по умолчанию.
+
+    :param telegram_token: Токен Telegram-бота
+    :param api_base_url: Базовый URL backend API
+    :param request_timeout: Таймаут HTTP-запросов к backend в секундах
+    :param debug: Флаг включения отладочного режима (влияет на уровень логирования)
+    :param http_proxy: URL HTTP-прокси (опционально, используется в loader.py)
+    :param https_proxy: URL HTTPS-прокси (опционально)
+    """
+
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
     telegram_token: str
