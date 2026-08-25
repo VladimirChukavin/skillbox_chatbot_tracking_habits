087c7db82275b6e25c64b9ea5cb57f75740a9020
VladimirChukavin <dartmorgot@yandex.ru>
2026-08-25 14:46:35 +1000

    Добавил описание функций.

--- bot/handlers/help_handlers.py
+++ bot/handlers/help_handlers.py
@@ -1,3 +1,10 @@
+"""
+Обработчик команды /help и текстового сообщения "Справка".
+
+Содержит функцию регистрации хендлера, который формирует и отправляет
+пользователю список доступных команд бота.
+"""
+
 from telebot import TeleBot
 from telebot.types import Message
 from telebot.apihelper import ApiException
@@ -7,9 +14,30 @@ from bot.config import DEFAULT_COMMANDS
 
 
 def register_help_handler(bot: TeleBot) -> None:
+    """
+    Зарегистрировать обработчик для команды /help и слова "Справка".
+
+    :param bot: Экземпляр Telegram-бота
+    :type bot: TeleBot
+    :return: Ничего не возвращает
+    :rtype: None
+    """
+
     @bot.message_handler(commands=["help"])
     @bot.message_handler(func=lambda message: message.text == "Справка")
     def handle_help(message: Message) -> None:
+        """
+        Отправить пользователю список доступных команд.
+
+        Формирует текст справки на основе DEFAULT_COMMANDS из конфигурации.
+
+        :param message: Входящее сообщение от пользователя
+        :type message: Message
+        :raises ApiException: При ошибке отправки сообщения в Telegram
+        :return: Ничего не возвращает
+        :rtype: None
+        """
+
         try:
             commands = [f"/{command} - {desc}" for command, desc in DEFAULT_COMMANDS]
             text = (
