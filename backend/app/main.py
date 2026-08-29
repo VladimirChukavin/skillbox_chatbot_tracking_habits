d38f422ad37db02a87ace90f3e78f25baf77e142
VladimirChukavin <dartmorgot@yandex.ru>
2026-08-29 22:42:12 +1000

    Мелкие правки.

--- backend/app/main.py
+++ backend/app/main.py
@@ -39,7 +39,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(
     title="Habit tracker API",
     description="Бэкенд чат-бота для трекинга привычек",
-    version="0.1.0",
+    version="1.0.0",
     lifespan=lifespan,
 )
 
