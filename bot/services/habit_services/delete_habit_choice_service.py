9bbda606946e1c50413989e7c70e9b0c596463e3
VladimirChukavin <dartmorgot@yandex.ru>
2026-08-31 21:38:43 +1000

    Изменил код файла, теперь бот не удаляет привычку сразу, а запрашивает подтверждение у пользователя.

--- bot/services/habit_services/delete_habit_choice_service.py
+++ bot/services/habit_services/delete_habit_choice_service.py
@@ -1,27 +1,30 @@
 """
-Сервис обработки выбора привычки для удаления.
+Сервис обработки подтверждения удаления привычки.
 
 Содержит функцию, которая вызывается при нажатии inline-кнопки
 удаления конкретной привычки. Функция извлекает идентификатор
-привычки из callback-данных, отправляет запрос на удаление
-через API-клиент и обновляет сообщение с результатом.
+привычки из callback-данных, отправляет запрос на подтверждение
+удаление через API-клиент и обновляет сообщение с результатом.
 """
 
 from telebot import TeleBot
 from telebot.types import CallbackQuery
 
-from bot.api.api_client import api_client
+from bot.keyboards.delete_confirmation_keyboard import (
+    build_delete_confirmation_keyboard,
+)
+from bot.states import DeleteHabitsStates
 
 
 def show_delete_habit_choice(bot: TeleBot, call: CallbackQuery) -> None:
     """
-    Обработать выбор привычки для удаления.
+    Обработать подтверждение удаления привычки.
 
     Извлекает идентификатор привычки из callback-данных формата
-    "delete:<habit_id>". Отправляет запрос на удаление через
-    API-клиент. При успешном удалении сбрасывает FSM-состояние
-    пользователя и заменяет текст исходного сообщения на уведомление
-    об успехе. При неудаче отправляет в чат сообщение об ошибке.
+    "delete:<habit_id>". Отправляет запрос на подтверждение удаления
+    через API-клиент. При этом выводит текст сообщения для подтверждения
+    удаления или отмены действия. Также выводит клавиатуру с кнопками
+    выбора - подтвердить или отменить действие.
 
     :param bot: Экземпляр Telegram-бота
     :type bot: TeleBot
@@ -34,12 +37,18 @@ def show_delete_habit_choice(bot: TeleBot, call: CallbackQuery) -> None:
     habit_id = int(call.data.split(":")[1])
     telegram_id = call.from_user.id
 
-    if api_client.delete_habit(telegram_id, habit_id):
-        bot.delete_state(telegram_id, call.message.chat.id)
-        bot.edit_message_text(
-            "🗑 Привычка удалена.",
-            chat_id=call.message.chat.id,
-            message_id=call.message.message_id,
-        )
-    else:
-        bot.send_message(telegram_id, "❌ Не удалось удалить привычку.")
+    with bot.retrieve_data(telegram_id, call.message.chat.id) as data:
+        data["habit_id_to_delete"] = habit_id
+
+    bot.set_state(
+        telegram_id,
+        DeleteHabitsStates.waiting_for_deletion_confirmation,
+        call.message.chat.id,
+    )
+
+    bot.edit_message_text(
+        f'Вы уверены, что хотите удалить привычку "{habit_id}"?',
+        chat_id=call.message.chat.id,
+        message_id=call.message.message_id,
+        reply_markup=build_delete_confirmation_keyboard(),
+    )
