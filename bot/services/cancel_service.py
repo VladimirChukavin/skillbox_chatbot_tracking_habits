e99e6e5e61802dd6a526b9c9bbdf0e3b20542020
VladimirChukavin <dartmorgot@yandex.ru>
2026-09-01 21:58:39 +1000

    Изменил код, добавил проверку входного параметра call_or_message.

--- bot/services/cancel_service.py
+++ bot/services/cancel_service.py
@@ -1,11 +1,15 @@
 from telebot import TeleBot
-from telebot.types import CallbackQuery
+from telebot.types import CallbackQuery, Message
 
 
-def show_cancel(bot: TeleBot, call: CallbackQuery) -> None:
-    telegram_id = call.from_user.id
-    chat_id = call.message.chat.id
+def show_cancel(bot: TeleBot, call_or_message: CallbackQuery | Message) -> None:
+    if hasattr(call_or_message, "from_user"):
+        telegram_id = call_or_message.from_user.id
+        chat_id = call_or_message.message.chat.id
+        bot.answer_callback_query(call_or_message.id)
+    else:
+        telegram_id = call_or_message.from_user.id
+        chat_id = call_or_message.chat.id
 
     bot.delete_state(telegram_id, chat_id)
     bot.send_message(telegram_id, "Действие отменено.")
-    bot.answer_callback_query(call.id)
