0504ca0176d853afe883c51ed0ed87c544c72042
VladimirChukavin <dartmorgot@yandex.ru>
2026-09-01 22:05:12 +1000

    Добавил обработку исключений.

--- bot/services/auth_services/start_service.py
+++ bot/services/auth_services/start_service.py
@@ -6,6 +6,7 @@
 либо инициирует процесс регистрации.
 """
 
+from loguru import logger
 from telebot import TeleBot
 from telebot.types import Message
 
@@ -34,18 +35,26 @@ def show_start(bot: TeleBot, message: Message) -> None:
 
     telegram_id = message.from_user.id
 
-    if token_storage.get_tokens(telegram_id) is not None:
+    try:
+
+        if token_storage.get_tokens(telegram_id) is not None:
+            bot.send_message(
+                telegram_id,
+                "Вы уже авторизованы. Используйте меню ниже.",
+                reply_markup=build_main_menu_keyboard(),
+            )
+            return
+
+        bot.set_state(
+            telegram_id, RegistrationStates.waiting_for_full_name, message.chat.id
+        )
         bot.send_message(
             telegram_id,
-            "Вы уже авторизованы. Используйте меню ниже.",
-            reply_markup=build_main_menu_keyboard(),
+            "Привет! Я бот для трекинга привычек. \nВведите ваше имя:",
         )
-        return
-
-    bot.set_state(
-        telegram_id, RegistrationStates.waiting_for_full_name, message.chat.id
-    )
-    bot.send_message(
-        telegram_id,
-        "Привет! Я бот для трекинга привычек. \nВведите ваше имя:",
-    )
+    except Exception as error:
+        logger.error("Ошибка выполнения команды /start: {}", error)
+        bot.send_message(
+            telegram_id,
+            "Что-то пошло не так. Попробуйте ещё раз.",
+        )
