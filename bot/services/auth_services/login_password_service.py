32c31f9ebbb55c590947655f5217aba149b88249
VladimirChukavin <dartmorgot@yandex.ru>
2026-09-01 22:03:04 +1000

    Добавил более специфический тип исключения, переменную chat_id.

--- bot/services/auth_services/login_password_service.py
+++ bot/services/auth_services/login_password_service.py
@@ -8,6 +8,7 @@
 from loguru import logger
 from telebot import TeleBot
 from telebot.types import Message
+from telebot.apihelper import ApiException
 
 from bot.api.api_client import api_client
 from bot.keyboards.main_menu_keyboard import build_main_menu_keyboard
@@ -31,21 +32,22 @@ def show_login_password(bot: TeleBot, message: Message) -> None:
 
     password = message.text.strip() if message.text else ""
     telegram_id = message.from_user.id
+    chat_id = message.chat.id
 
     try:
-        bot.delete_message(message.chat.id, message.message_id)
-    except Exception as error:
+        bot.delete_message(chat_id, message.message_id)
+    except ApiException as error:
         logger.warning("Не удалось удалить сообщение с паролем: {}", error)
 
     if not password:
         bot.send_message(
             telegram_id, "Пароль не может быть пустым. Введите /login снова."
         )
-        bot.delete_state(telegram_id, message.chat.id)
+        bot.delete_state(telegram_id, chat_id)
         return
 
     token_data = api_client.login_user(telegram_id, password)
-    bot.delete_state(telegram_id, message.chat.id)
+    bot.delete_state(telegram_id, chat_id)
 
     if token_data is None:
         bot.send_message(telegram_id, "❌ Неверный пароль. Попробуйте /login снова.")
